b78106967e32c770632af3edddb1ff1d0fffd317
lunaticwicca <ulkutpl@icloud.com>
2022-02-12 23:11:55 +0000

    <votre message de commit>

--- main/02-intermediaire.js
+++ main/02-intermediaire.js
@@ -3,9 +3,24 @@
  * 
  * Exemple : [1, 2, 3] et ["apple", "orange", "banana"] renverra : [1, "apple", 2, "orange", 3, "banana"]
  */
-function combine(tableau1, tableau2) {
 
-}
+    function combine(tableau1, tableau2) {
+        var newArray = []
+        if (array1.length !== array2.length) return
+        for (var i = 0; i < array1.length; i++) {
+            newArray[i * 2] = array1[i];
+            newArray[i * 2 + 1] = array2[i];
+        }
+        return newArray;
+    
+    }
+    var array1Combine = [1, 2, 3, 4, 5];
+    var array2Combine = ['raspberry', 'apple', 'strawberry', 'pear', 'banana'];
+    console.log(combine(array1Combine, array2Combine))
+    
+
+
+
 
 /**
  * Programmer une fonction prenant en argument un tableau d'éléments et une valeur offset, et qui renvoie un tableau avec les valeurs du tableau en argument décalées de la valeur
